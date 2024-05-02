c9967fbb655fbf1df93b846332f03ddea1fb8e57
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2024-05-02 08:58:56 +0100

    KK

--- resources/views/refraction/create.blade.php
+++ resources/views/refraction/create.blade.php
@@ -123,6 +123,100 @@
             </tr>
         </tbody>
     </table>
+    <table class="table table-striped table-bordered">
+        <thead class="table-dark">
+            <th></th>
+            <th>NEAR</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td width="80%">RIGHT</td>
+                <td><input type="text" name="near_right" class="form-control"></td>
+            </tr>
+            <tr>
+                <td width="80%">LEFT</td>
+                <td><input type="text" name="near_left" class="form-control"></td>
+            </tr>
+        </tbody>
+    </table>
+    <h4>Present Glasses</h4>
+    <table class="table table-striped table-bordered">
+        <thead class="table-dark">
+            <th></th>
+            <th>SPH</th>
+            <th>CYL</th>
+            <th>AXIS</th>
+            <th>PRISM</th>
+            <th>BASE</th>
+            <th>VA</th>
+            <th>ADD</th>
+            <th>VA</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td width="70%">RIGHT</td>
+                <td><input type="text" name="sph_right" class="form-control"></td>
+                <td><input type="text" name="cyl_right" class="form-control"></td>
+                <td><input type="text" name="axis_right" class="form-control"></td>
+                <td><input type="text" name="prism_right" class="form-control"></td>
+                <td><input type="text" name="base_right" class="form-control"></td>
+                <td><input type="text" name="va_right" class="form-control"></td>
+                <td><input type="text" name="add_right" class="form-control"></td>
+                <td><input type="text" name="va2_right" class="form-control"></td>
+            </tr>
+            <tr>
+                <td width="70%">LEFT</td>
+                <td><input type="text" name="sph_left" class="form-control"></td>
+                <td><input type="text" name="cyl_left" class="form-control"></td>
+                <td><input type="text" name="axis_left" class="form-control"></td>
+                <td><input type="text" name="prism_left" class="form-control"></td>
+                <td><input type="text" name="base_left" class="form-control"></td>
+                <td><input type="text" name="va_left" class="form-control"></td>
+                <td><input type="text" name="add_left" class="form-control"></td>
+                <td><input type="text" name="va2_left" class="form-control"></td>
+            </tr>
+        </tbody>
+    </table>
+    <h4>Auto Refraction</h4>
+    <table class="table table-striped table-bordered">
+        <thead class="table-dark">
+            <th></th>
+            <th>AUTO REFRACTION</th>
+            <th>VA</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td width="50%">RIGHT</td>
+                <td><input type="text" name="auto_refraction_right" class="form-control"></td>
+                <td><input type="text" name="va_right" class="form-control"></td>
+            </tr>
+            <tr>
+                <td width="50%">LEFT</td>
+                <td><input type="text" name="auto_refraction_left" class="form-control"></td>
+                <td><input type="text" name="va_left" class="form-control"></td>
+            </tr>
+        </tbody>
+    </table>
+    <h4>Retinoscopy Findings</h4>
+    <table class="table table-striped table-bordered">
+        <thead class="table-dark">
+            <th></th>
+            <th>AUTO REFRACTION</th>
+            <th>VA</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td width="50%">RIGHT</td>
+                <td><input type="text" name="auto_refraction_right" class="form-control"></td>
+                <td><input type="text" name="va_right" class="form-control"></td>
+            </tr>
+            <tr>
+                <td width="50%">LEFT</td>
+                <td><input type="text" name="auto_refraction_left" class="form-control"></td>
+                <td><input type="text" name="va_left" class="form-control"></td>
+            </tr>
+        </tbody>
+    </table>
     <div class="col-12 text-center">
         <button type="submit" class="btn btn-primary me-sm-3 me-1">Submit</button>
         <button type="reset" class="btn btn-label-secondary" data-bs-dismiss="modal"
